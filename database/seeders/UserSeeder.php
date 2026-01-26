49a700261d780973eb7d217fdbc83206af3f0d63
Fadhal <fadhalnurulazmi75@gmail.com>
2026-01-26 08:10:09 +0700

    fix database seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Petugas Lab',
+            'email' => 'petugas@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'petugas',
+        ]);
+    }
+}
